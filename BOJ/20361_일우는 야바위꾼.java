eec967e687e0dbe5e767e0a52949a87db8f348f0
ImAYeon <48500341+ImAYeon@users.noreply.github.com>
2021-02-20 15:11:43 +0900

    Create 20361_일우는 야바위꾼.java

---
+++ BOJ/20361_일우는 야바위꾼.java
@@ -0,0 +1,31 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_일우는야바위꾼_20361 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken()); 
+		int K = Integer.parseInt(st.nextToken()); 
+
+		for (int i = 0; i < K; i++) { 
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			if (A == X) { 
+				X = B; 
+			} else if (B == X) { 
+				X = A; 
+			}
+		}
+
+		System.out.print(X);
+	}
+}
