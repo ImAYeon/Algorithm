3b6afe7157a2daace10bee5cde53460dc78eea29
ImAYeon <48500341+ImAYeon@users.noreply.github.com>
2021-06-21 15:26:55 +0900

    Create 문자사각형1_1307.java

---
+++ JUNGOL/Beginner_Coder/도형만들기1/문자사각형1_1307.java
@@ -0,0 +1,31 @@
+package 도형만들기1;
+
+import java.util.Scanner;
+
+public class JUNGOL_문자사각형1_1307 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); // 정사각형 한 변의 길이
+		
+		char[][] arr = new char[n][n];
+		
+		char ch = 'A';
+		
+		for(int i = n-1; i >= 0; i--) {
+			for(int j = n-1; j >= 0; j--) {
+				arr[j][i] = ch++;
+				if(ch-1 == 'Z') ch = 'A';
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
