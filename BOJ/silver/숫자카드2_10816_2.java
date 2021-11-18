725eb1d373374015b5bd4a46e7ef4e4c521577c1
ImAYeon <48500341+ImAYeon@users.noreply.github.com>
2021-11-18 18:36:19 +0900

    Create 숫자카드2_10816_2.java

---
+++ BOJ/silver/숫자카드2_10816_2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		int[] num = new int[20000001];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while(size --> 0) {
+			num[Integer.parseInt(st.nextToken()) + 10000000]++;
+		}
+		
+		size = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			sb.append(num[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
