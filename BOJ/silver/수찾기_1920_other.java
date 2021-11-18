3aa0fbcaff1af5ccc617addff8791f915ab8d729
ImAYeon <48500341+ImAYeon@users.noreply.github.com>
2021-11-18 17:28:37 +0900

    Create 수찾기_1920_other.java

---
+++ BOJ/silver/수찾기_1920_other.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+ 
+class Main{
+    static public void main(String[] args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        HashSet<String> data=new HashSet<String>();
+        StringBuilder answer=new StringBuilder();
+        
+        int n=Integer.parseInt(br.readLine());
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++) data.add(st.nextToken());
+        
+        int m=Integer.parseInt(br.readLine());
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<m;i++){
+            answer.append(data.contains(st.nextToken())?1:0).append("\n");
+        }
+        System.out.println(answer);
+    }
+}
