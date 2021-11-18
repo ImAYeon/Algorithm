0cda68b09a10f881e4c9ed29963bc8be3c690688
ImAYeon <48500341+ImAYeon@users.noreply.github.com>
2021-11-18 18:38:46 +0900

    Update 숫자카드2_10816_2.java

--- BOJ/silver/숫자카드2_10816_2.java
+++ BOJ/silver/숫자카드2_10816_2.java
@@ -1,28 +1,29 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		int size = Integer.parseInt(br.readLine());
-		
-		int[] num = new int[20000001];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(size --> 0) {
-			num[Integer.parseInt(st.nextToken()) + 10000000]++;
-		}
-		
-		size = Integer.parseInt(br.readLine());
-		
-		st = new StringTokenizer(br.readLine());
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int[] arr = new int[20000001];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		while(size --> 0) {
-			sb.append(num[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
+
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[Integer.parseInt(st.nextToken()) + 10000000]++;
 		}
+
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			sb.append(arr[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
+		}
+
 		System.out.println(sb);
+        	br.close();
 	}
+
 }
